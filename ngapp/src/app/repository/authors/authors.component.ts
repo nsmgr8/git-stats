99648b55fdf76a22f0e782a1019d7675731c35d0
M Nasimul Haque <nasim.haque@gmail.com>
2018-06-03 01:30:28 +0100

    authors of the period lists

--- ngapp/src/app/repository/authors/authors.component.ts
+++ ngapp/src/app/repository/authors/authors.component.ts
@@ -10,13 +10,14 @@ import { RepositoriesService } from '../../services/repositories.service';
 })
 export class AuthorsComponent implements OnInit {
     subscription;
-    activity;
     authors;
     repo;
     order = {
         field: 'commits',
         direction: 1
     };
+
+    author_of_the_periods;
 
     constructor(
         public repoService: RepositoriesService,
@@ -39,7 +40,14 @@ export class AuthorsComponent implements OnInit {
     }
 
     setRepoActivity(data) {
-        this.activity = data;
+        this.setAuthors(data);
+        this.author_of_the_periods = [
+            'weekly', 'monthly', 'yearly'
+        ].map(period => ({type: period, value: this.setAuthorOfThePeriod(data, period)}));
+        console.log(this.author_of_the_periods);
+    }
+
+    setAuthors(data) {
         const authors = Object.keys(data.by_authors);
         this.authors = authors.map(a => {
             let {commits, insertions, deletions} = data.by_authors[a].yearly;
@@ -71,5 +79,38 @@ export class AuthorsComponent implements OnInit {
         this.authors = this.authors.sort((a, b) => {
             return this.order.direction * (b[field] - a[field]);
         });
+    }
+
+    setAuthorOfThePeriod(data, period) {
+        const authors = Object.keys(data.by_authors);
+        const rows = {};
+        authors.forEach(a => {
+            const {commits, insertions, deletions} = data.by_authors[a][period];
+            const keys = Object.keys(commits);
+            keys.forEach(key => {
+                const row = {
+                    author: a,
+                    commits: commits[key],
+                    insertions: insertions && insertions[key],
+                    deletions: deletions && deletions[key]
+                };
+
+                if (rows[key]) {
+                    rows[key].push(row);
+                } else {
+                    rows[key] = [row];
+                }
+            });
+        });
+
+        return Object.keys(rows)
+            .sort((a, b) => {
+                return b.localeCompare(a);
+            }).map(x => {
+                return {
+                    period: x,
+                    data: rows[x].sort((a, b) => b.commits - a.commits)
+                };
+            });
     }
 }
