96c10e42408192971a671f98720e25b5e27c7608
M Nasimul Haque <nasim.haque@gmail.com>
2018-06-09 19:47:20 +0100

    top 5 lines of code pie chart

--- ngapp/src/app/repository/lines/lines.component.ts
+++ ngapp/src/app/repository/lines/lines.component.ts
@@ -14,6 +14,8 @@ export class LinesComponent implements OnInit {
     lines_total;
     order = 'code';
 
+    chart_options;
+
     constructor(
         public repoService: RepositoriesService,
         public route: ActivatedRoute
@@ -45,15 +47,65 @@ export class LinesComponent implements OnInit {
                     total: d.blank + d.comment + d.code
                 };
             });
+
         this.lines_total = {
             ...data.lines.header,
             ...data.lines.SUM,
             total: data.lines.SUM.blank + data.lines.SUM.comment + data.lines.SUM.code
         };
+
+        this.setChart();
     }
 
     toggleOrder(field) {
         this.order = field;
         this.lines = this.lines.sort((a, b) => b[field] - a[field]);
+    }
+
+    setChart() {
+        this.chart_options = {
+            series: [
+                {
+                    name: 'Lines of code',
+                    type: 'pie',
+                    label: {
+                        normal: {
+                            formatter: '{a|{a}}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+                            backgroundColor: '#eee',
+                            borderColor: '#aaa',
+                            borderWidth: 1,
+                            borderRadius: 4,
+                            rich: {
+                                a: {
+                                    color: '#999',
+                                    lineHeight: 22,
+                                    align: 'center'
+                                },
+                                hr: {
+                                    borderColor: '#aaa',
+                                    width: '100%',
+                                    borderWidth: 0.5,
+                                    height: 0
+                                },
+                                b: {
+                                    fontSize: 16,
+                                    lineHeight: 33
+                                },
+                                per: {
+                                    color: '#eee',
+                                    backgroundColor: '#334455',
+                                    padding: [2, 4],
+                                    borderRadius: 2
+                                }
+                            }
+                        }
+                    },
+                    radius: [0, '55%'],
+                    data: this.lines.slice(0, 5).map((x, i) => ({
+                        value: x.code, name: x.lang, selected: i === 0
+                    }))
+                }
+            ]
+        };
     }
 }
